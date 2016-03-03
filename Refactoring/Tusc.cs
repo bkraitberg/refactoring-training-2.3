a827ae88338245c2c522084c57abdf5929bcf513
mahfuzr <cmk7702@varian.com>
2016-03-03 13:10:50 -0600

    Commit 4

--- Refactoring/Tusc.cs
+++ Refactoring/Tusc.cs
@@ -197,7 +197,9 @@ namespace Refactoring
 	        {
 	            Console.WriteLine("Enter the product number:");
                 string ProductNumberEntered = Console.ReadLine();
-                if (ProductNumberEntered.Equals("quit")) return ProductCount + 1; //(ProductCount + 1) is the code to quit
+                if (ProductNumberEntered!=null) 
+                    if(ProductNumberEntered.Equals("quit")) 
+                        return ProductCount + 1; //(ProductCount + 1) is the code to quit
                 if (validateProduct(ProductNumberEntered, out productNumber))
                 {
                    break;
@@ -237,6 +239,8 @@ namespace Refactoring
             for (int i = 0; i < ProductCount; i++)
             {
                 Product prod = ProductList[i];
+
+                if(prod.Qty>0)
                 Console.WriteLine(i + 1 + ": " + prod.Name + " (" + prod.Price.ToString("C") + ")");
             }
             Console.WriteLine("Type quit to Exit the application");
